8bcf427b2aa55e946c88aadb82d11e488a195d7c
Vikram Singh Saini <sspl.vssaini@gmail.com>
2021-03-03 06:57:45 +0530

    Add project files.

---
+++ ListLocalUsers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.DirectoryServices;
+using System.Linq;
+
+namespace ListLocalUsers
+{
+    class Program
+    {
+        static void Main()
+        {
+            ListUser();
+
+            Console.ReadKey();
+        }
+
+        private static void ListUser()
+        {
+           var winNtPath = string.Format("WinNT://{0}", "W2K8SRV1");
+
+            using (var computerEntry = new DirectoryEntry(winNtPath,"Administrator","Pass99"))
+            {
+                var userNames = from DirectoryEntry childEntry in computerEntry.Children
+                                where childEntry.SchemaClassName == "Group"
+                                select childEntry.Name;
+
+                foreach (var name in userNames)
+                    Console.WriteLine(name);
+            }      
+        }
+    }
+}
